762550606415f67bcc565e1befa86d613952ea03
Hugo Blanc <hugo.blanc@agicap.com>
2023-04-14 01:01:23 +0200

    fix: cors sisue

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({ origin: '*' });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1:4200',
+      'http://localhost:4200',
+      'https://accelerator.inv.agicapcollect.com',
+    ],
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
