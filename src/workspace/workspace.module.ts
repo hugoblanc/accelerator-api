2b0aa484eb8cafd27193db283e2b34aec21de88a
Pierre Bazin <bazinpierre76@icloud.com>
2023-08-02 19:41:01 +0200

    feat: add member in ws and team

--- src/workspace/workspace.module.ts
+++ src/workspace/workspace.module.ts
@@ -1,10 +1,13 @@
 import { WorkspaceService } from './workspace.service';
 import { WorkspaceController } from './workspace.controller';
 import { Module } from '@nestjs/common';
+import { UserService } from '../user/application/user.service';
+import { TeamService } from '../team/team.service';
+import { AuthService } from '../core/security/auth/auth.service';
 
 @Module({
   imports: [],
   controllers: [WorkspaceController],
-  providers: [WorkspaceService],
+  providers: [WorkspaceService, UserService, TeamService, AuthService],
 })
 export class WorkspaceModule {}
